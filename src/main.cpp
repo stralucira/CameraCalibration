018f99ad0fa69f56261c1da54b5c95e00c6a37e7
stralucira <baoguz@gmail.com>
2018-02-13 21:09:58 +0100

    Created a calibration routine for 4 snapshots from the camera (press P to snap).

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,17 @@
 using namespace cv;
 using namespace std;
 
-void startVideoCapture();
 void calcBoardCornerPositions(Size boardSize, float squareSize, vector<Point3f>& corners);
 
+int boardCount = 4; //Number of boards to be found before calibration.
 
 int main()
 {
-	float aspectRatio;
-
+	float aspectRatio = 16.0f / 9.0f;
 	float squareSize = 1.0f;
 
 	vector<Mat> rvecs, tvecs;
+	int successes = 0;
 
 	//Create a 3x3 identity matrix
 	Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
@@ -31,36 +31,60 @@ int main()
 	Mat distCoeffs = Mat::zeros(8, 1, CV_64F);
 	
 	// Considering the aspect ratio is fixed (CALIB_FIX_ASPECT_RATIO) set fx/fy
-	
-	
-	//startVideoCapture();
-
 	Size testPatternSize(6, 9);
-	vector<Point2f> pointBuffer;
+	int cornersPerBoard = 6 * 9;
 
-	cv::Mat img = cv::imread("data/chess_test.jpg");
+	//CvCapture* capture = cvCreateCameraCapture(0);
+	VideoCapture stream1(0);
+	Mat cameraFrame;
+	Mat cameraFrame_bw;
 
-	aspectRatio = (float) img.size().width / (float) img.size().height;
-
+	stream1.read(cameraFrame);
+	aspectRatio = (float)cameraFrame.size().width / (float)cameraFrame.size().height;
 	cameraMatrix.at<double>(0, 0) = aspectRatio;
+	cameraMatrix.at<double>(1, 1) = 1.0f;
 
-	bool patternFound = findChessboardCorners(img, testPatternSize, pointBuffer,
-		CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
-		+ CALIB_CB_FILTER_QUADS);
+	cvNamedWindow("Camera");
+	cvNamedWindow("Pattern");
 
-	drawChessboardCorners(img, testPatternSize, pointBuffer, patternFound);
-	cv::imshow("Chess Test", img);
-	cv::waitKey(0);
+	if (!stream1.isOpened()) { //check if video device is initialised
+		cout << "cannot open camera";
+	}
+
+	vector<Point2f> pointBuffer;
 
 	vector<vector<Point3f>> objectPoints(1);
 	vector<vector<Point2f>> imagePoints;
 
-	imagePoints.push_back(pointBuffer);
-
 	calcBoardCornerPositions(testPatternSize, squareSize, objectPoints[0]);
+
+	//Find boardCount number of patterns and record the coordinates.
+	while (successes < boardCount) {
+		stream1.read(cameraFrame);
+		imshow("Camera", cameraFrame);
+
+		//Try to find the chessboard pattern when "p" is pressed
+		if (waitKey(1) == 'p') {
+			bool patternFound = findChessboardCorners(cameraFrame, testPatternSize, pointBuffer, 
+				CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FILTER_QUADS);
+
+			if (patternFound) {
+				cvtColor(cameraFrame, cameraFrame_bw, CV_BGR2GRAY);
+				cornerSubPix(cameraFrame_bw, pointBuffer, Size(11, 11), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+
+				drawChessboardCorners(cameraFrame, testPatternSize, pointBuffer, patternFound);
+				cv::imshow("Pattern", cameraFrame);
+				
+				imagePoints.push_back(pointBuffer);
+
+				successes++;
+			}
+		}
+	}
+	
 	objectPoints.resize(imagePoints.size(), objectPoints[0]);
 
-	double rms = calibrateCamera(objectPoints, imagePoints, img.size(), cameraMatrix,
+	double rms = calibrateCamera(objectPoints, imagePoints, cameraFrame.size(), cameraMatrix,
 		distCoeffs, rvecs, tvecs, CV_CALIB_USE_INTRINSIC_GUESS);
 
 	cout << "Re-projection error reported by calibrateCamera: " << rms << endl;
@@ -75,17 +99,3 @@ void calcBoardCornerPositions(Size boardSize, float squareSize, vector<Point3f>&
 			//Set z=0 for every point, considering the chessboard is planar.
 			corners.push_back(Point3f(float(j*squareSize), float(i*squareSize), 0));
 }
-void startVideoCapture() {
-	VideoCapture stream1(0);
-
-	if (!stream1.isOpened()) { //check if video device is initialised
-		cout << "cannot open camera";
-	}
-
-	while (true) {
-		Mat cameraFrame;
-		stream1.read(cameraFrame);
-		imshow("cam", cameraFrame);
-		waitKey(30);
-	}
-};
